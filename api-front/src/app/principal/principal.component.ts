aecb40f8b5e3e6f88b846f21755b8cbb81163a5a
yurilsn <luizyuri57@gmail.com>
2024-01-06 17:04:12 -0300

    Configurando Http para requisições

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
