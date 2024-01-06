8d0d0233a8a59fc5319fdd58072d83a26788fd99
yurilsn <luizyuri57@gmail.com>
2024-01-06 16:24:02 -0300

    Configurando css, adcionando imagem e ajuste de campos

--- api-front/src/app/app.component.ts
+++ api-front/src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { PrincipalComponent } from './principal/principal.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, 
+            RouterOutlet,
+            PrincipalComponent
+            ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
