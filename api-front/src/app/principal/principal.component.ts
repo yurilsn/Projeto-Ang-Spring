2916d2efc75d6dae6364d3e76798e5aad73c932d
yurilsn <luizyuri57@gmail.com>
2024-01-06 18:03:10 -0300

    Criando requisição GET na api front

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Cliente } from './domain/Cliente';
+import { ClienteService } from '../service/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -11,6 +13,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class PrincipalComponent {
 
+
+  //Visibilidade de botões
   botaoCadastro:boolean = true; 
+
+  // Json Clientes
+  clientes:Cliente[] = [];
+
+  //Construtor do service
+  constructor(private service:ClienteService){}
+
+  //Get
+  get(): void {
+     this.service.findAll()
+     .subscribe(ListaClientes => this.clientes = ListaClientes);
+  }
 
 }
--- api-front/src/app/service/cliente.service.ts
+++ api-front/src/app/service/cliente.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../principal/domain/Cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private url:string = 'http://localhost:8080/api/Cliente';
+
+  constructor(private http:HttpClient) { }
+
+  findAll(): Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
 }
