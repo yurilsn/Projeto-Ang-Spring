dc596992bab4809478b3dd587b56c76df6307b02
yurilsn <luizyuri57@gmail.com>
2024-01-06 16:52:43 -0300

    Adicionando condicional aos botões

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+
+  botaoCadastro:boolean = true; 
 
 }
